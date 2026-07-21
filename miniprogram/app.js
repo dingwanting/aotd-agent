dddcaa7c4db810e3d72f7c905c64fa902a2a5b6e
dingwanting <422781640@qq.com>
2026-07-21 11:40:25 +0800

    feat: switch mini program API to callContainer

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,3 +1,21 @@
+const { CLOUD_ENV_ID, USE_CLOUD_CONTAINER } = require("./utils/config");
+
 App({
+  onLaunch() {
+    if (!USE_CLOUD_CONTAINER) {
+      return;
+    }
+
+    if (!wx.cloud) {
+      console.error("当前基础库不支持 wx.cloud，请升级微信客户端或基础库版本。");
+      return;
+    }
+
+    wx.cloud.init({
+      env: CLOUD_ENV_ID,
+      traceUser: true,
+    });
+  },
+
   globalData: {},
 });
